818de8efe5531f2552214aea0f4d629311527f7a
FLAVIO SANTARELLI <145938765+PROFSANTARELLI@users.noreply.github.com>
2026-06-25 16:29:25 -0300

    Add User and Task models to backend

---
+++ backend/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from backend.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    priority = Column(String, nullable=False)
+    status = Column(String, default="PENDING")
